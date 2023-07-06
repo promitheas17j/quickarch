1d26391642a9a03cede77990dae4a27b9f1bb156
Martin Aziz <maziz17j@gmail.com>
2023-07-06 04:56:29 +0300

    Make minor changes

--- setup-arch.sh
+++ setup-arch.sh
@@ -2,16 +2,22 @@
 
 # Enable NetworkManager to start on boot
 systemctl enable NetworkManager
+
 # Make NetworkManager is running
 systemctl start NetworkManager
+
 # Create a couple directories for mounting usb's
 mkdir -p /mnt/usb_1/ /mnt/usb_2/
+
 # Install reflector with all default options
 pacman -S --noconfirm reflector
+
 # Backup mirrorlist
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
 # Run reflector to get the best pacman mirrors
 reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
 # Update and upgrade the system with all default options
 pacman -Syyu --noconfirm
 
@@ -21,7 +27,7 @@ pacman -S zsh mesa sddm xorg-server xorg-apps xorg-xinit xf86-video-amdgpu neovi
 # Export default editor to be neovim
 export EDITOR=nvim
 
-# Add user
+# Create user
 useradd -m -g users -s /bin/zsh mart
 usermod -aG wheel root
 usermod -aG wheel mart
@@ -35,4 +41,4 @@ cd yay
 makepkg -si --noconfirm
 
 # Installing packages from AUR
-yay -S brave-bin pavucontrol anamnesis
+yay -S brave-bin pavucontrol 
